2a72acffbf550601e4f26e6ec0db42cdbf4a0548
Alex iOS <alex.ovechko@softensy.com>
2018-06-12 12:07:13 +0300

    Added AESCipher.

--- TFSecured/tf/encryption/AESEncryptor.cpp
+++ TFSecured/tf/encryption/AESEncryptor.cpp
@@ -21,6 +21,13 @@ inline Dest* __convertData(const Src* data, const size_t elmCount, size_t *retur
     return result;
 }
 
+inline size_t tensorElmCount(const tensorflow::Tensor &tensor){
+    size_t size = 0;
+    auto shape = tensor.shape();
+    size = shape.dims();
+    return size;
+}
+
 AESEncryptor::AESEncryptor(const std::string &key)
              : _key(key) {
      AES_init_ctx(&_aesCtx, (const uint8_t*)key.c_str());
@@ -37,18 +44,22 @@ template<typename In>
 const tensorflow::Tensor AESEncryptor::encrypt(const tensorflow::Tensor& input) {
     tensorflow::Tensor out(input.dtype(), input.shape());
 
-    const size_t tensorElmCount = input.AllocatedBytes()/sizeof(In);
+    const size_t elmCount = tensorElmCount(input);
     size_t bytesCount = 0;
-    uint8_t *bytes = __convertData<In, uint8_t>(input.flat<In>().data(), tensorElmCount, &bytesCount);
+    auto tensorBytes = input.flat<In>().data();
+    uint8_t *bytes = __convertData<In, uint8_t>(tensorBytes, elmCount, &bytesCount);
     AES_ECB_encrypt(&_aesCtx, bytes);
-
+    tensorBytes = __convertData<uint8_t, In>(bytes, bytesCount,  nullptr);
     return out;
 }
 
 template<typename In>
 const tensorflow::Tensor AESEncryptor::decrypt(const tensorflow::Tensor& input) {
     tensorflow::Tensor out(input.dtype(), input.shape());
-    //TODO:
+    const size_t tensorElmCount = input.AllocatedBytes()/sizeof(In);
+    size_t bytesCount = 0;
+    uint8_t *bytes = __convertData<In, uint8_t>(input.flat<In>().data(), tensorElmCount, &bytesCount);
+    AES_ECB_decrypt(&_aesCtx, bytes);
     return out;
 }
 
