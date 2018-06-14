91931d7c0f893f599531ed9de9b82d08e1ca16e8
Alex iOS <alex.ovechko@softensy.com>
2018-06-14 16:40:01 +0300

    Added tests.

---
+++ python/test.py
@@ -0,0 +1,26 @@
+from encrypt_model import AESCipher
+
+
+
+def testAES():
+    KEY = 'DJM2GUH7TPNCRU4NVEJXX7QJE3BDBH'
+    cipher = AESCipher(KEY)
+    text = 'Some Text'
+    encrypted = cipher.encrypt(text)
+    
+    decrypted = cipher.decrypt(encrypted)
+    if decrypted != text:
+        raise Exception('Match error: "%s" != "%s"' % (text, decrypted))
+
+
+
+print('\n-----------------------------------')
+print('Tests:')
+
+tests = [testAES]
+
+for test in tests:
+    test()
+    print('Test "%s" is succeed\n' % (test.func_name))
+
+print('-----------------------------------\n')
