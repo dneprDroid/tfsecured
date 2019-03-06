33a498a9cd7de1139663475b0acc038534588c9a
Alex iOS <ovechko.056@gmail.com>
2019-03-07 01:09:10 +0200

    Refactoring.

--- TFSecured/GraphDefDecryptor.hpp
+++ TFSecured/GraphDefDecryptor.hpp
@@ -8,44 +8,21 @@
 
 #include <stdio.h>
 
-#include <tensorflow/core/framework/op.h>
-#include <tensorflow/core/framework/op_kernel.h>
-#include <tensorflow/core/public/session.h>
 
-#include <tensorflow/core/framework/shape_inference.h>
 #include <iostream>
 #include <fstream>
 
-#include <openssl/evp.h>
-#include <openssl/aes.h>
-#include <openssl/err.h>
-#include <openssl/sha.h>
+#include "GraphDefDecryptorInternals.hpp"
 
 using namespace tensorflow;
 
 namespace tfsecured {
     
-    #define CHECK_AES_STATUS(status, ctx, msg) if (status == 0) {                   \
-                                                EVP_CIPHER_CTX_cleanup(ctx);        \
-                                                EVP_CIPHER_CTX_free(ctx);           \
-                                                return errors::DataLoss(msg);       \
-                                               }
-    #define DEFAULT_KEY_SIZE       SHA256_DIGEST_LENGTH
-    
-    typedef std::array<uint8_t, DEFAULT_KEY_SIZE> KeyBytes;
-
-    
-    static const int32_t AES_INIT_VECTOR_SIZE = AES_BLOCK_SIZE;
-    
-    namespace internal  {
-        inline Status decryptAES(const KeyBytes &key_bytes,
-                                 std::vector<uint8_t> &input_content,
-                                 const uint32_t content_size);
-    }
     
     typedef std::function<Status(const KeyBytes &key_bytes,
                                  std::vector<uint8_t> &input_content,
                                  const uint32_t content_size)> Decryptor;
+    
     
     inline Status GraphDefDecrypt(const std::string &modelPath,
                                   GraphDef *graph,
@@ -81,6 +58,7 @@ namespace tfsecured {
         return Status::OK();
     }
     
+    
     inline Status GraphDefDecrypt(const std::string &modelPath,
                                   GraphDef *graph,
                                   const KeyBytes &keyByteArray) {
@@ -89,63 +67,19 @@ namespace tfsecured {
                                internal::decryptAES);
     }
     
+    
     inline Status GraphDefDecryptAES(const std::string &modelPath,
                                      GraphDef *graph,
                                      const std::string &key256) {
         KeyBytes hashKey;
         
-        SHA256_CTX sha256;
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256, key256.c_str(), key256.size());
-        SHA256_Final(hashKey.data(), &sha256);
+        internal::calculateSha256(key256.c_str(), key256.size(),
+                                  hashKey.data());
         
         return GraphDefDecrypt(modelPath,
                                graph, hashKey,
                                internal::decryptAES);
     }
  
-    inline Status internal::decryptAES(const KeyBytes &key_bytes,
-                                       std::vector<uint8_t> &input_content,
-                                       const uint32_t content_size) {
-        
-        /*** Initialization ***/
-        
-        EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-        
-        int status = 1;
-        
-        const std::vector<uint8_t> iv_bytes(input_content.begin(),
-                                            input_content.begin() + AES_INIT_VECTOR_SIZE);
-
-        EVP_CIPHER_CTX_init(ctx);
-        status = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key_bytes.data(), iv_bytes.data());
-        CHECK_AES_STATUS(status, ctx, "[OpenSSL] EVP_DecryptInit_ex Error");
-        
-        EVP_CIPHER_CTX_set_key_length(ctx, EVP_MAX_KEY_LENGTH);
-        
-        /*** Decryption ***/
-        
-        int p_len = (int)input_content.size();
-        int f_len = 0;
-        std::vector<unsigned char> result_buffer(p_len);
-        
-        status = EVP_DecryptUpdate(ctx,
-                                   result_buffer.data(), &p_len,
-                                   input_content.data() + AES_INIT_VECTOR_SIZE,
-                                   (int) input_content.size() - AES_INIT_VECTOR_SIZE);
-        CHECK_AES_STATUS(status, ctx, "[OpenSSL] EVP_DecryptUpdate Error");
-
-        status = EVP_DecryptFinal_ex(ctx, result_buffer.data() + p_len, &f_len);
-        CHECK_AES_STATUS(status, ctx, "[OpenSSL] EVP_DecryptFinal_ex Error");
-        
-        EVP_CIPHER_CTX_cleanup(ctx);
-        EVP_CIPHER_CTX_free(ctx);
-        
-        int result_len = p_len + f_len;
-        result_buffer.resize(result_len);
-        
-        input_content = result_buffer;
-        
-        return Status::OK();
-    }
+    
 }
---
+++ TFSecured/GraphDefDecryptorInternals.hpp
@@ -0,0 +1,103 @@
+//
+//  GraphDefDecryptorInternals.hpp
+//  TFSecured-iOS
+//
+//  Created by user on 3/7/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <tensorflow/core/framework/op.h>
+#include <tensorflow/core/framework/op_kernel.h>
+#include <tensorflow/core/public/session.h>
+
+#include <tensorflow/core/framework/shape_inference.h>
+
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/err.h>
+#include <openssl/sha.h>
+
+using namespace tensorflow;
+
+namespace tfsecured {
+
+
+    #define CHECK_AES_STATUS(status, ctx, msg) if (status == 0) {                   \
+                                                EVP_CIPHER_CTX_cleanup(ctx);        \
+                                                EVP_CIPHER_CTX_free(ctx);           \
+                                                return errors::DataLoss(msg);       \
+                                              }
+    #define DEFAULT_KEY_SIZE       SHA256_DIGEST_LENGTH
+
+    typedef std::array<uint8_t, DEFAULT_KEY_SIZE> KeyBytes;
+
+
+    static const int32_t AES_INIT_VECTOR_SIZE = AES_BLOCK_SIZE;
+
+    namespace internal {
+        
+        
+        inline void calculateSha256(const char *input, const size_t input_len,
+                                    unsigned char *out) {
+            SHA256_CTX sha256;
+            SHA256_Init(&sha256);
+            SHA256_Update(&sha256, input, input_len);
+            SHA256_Final(out, &sha256);
+        }
+        
+        inline Status decryptAES(const KeyBytes &key_bytes,
+                                 std::vector<uint8_t> &input_content,
+                                 const uint32_t content_size) {
+            
+            /*** Initialization ***/
+            
+            EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+            
+            int status = 1;
+            
+            const std::vector<uint8_t> iv_bytes(input_content.begin(),
+                                                input_content.begin() + AES_INIT_VECTOR_SIZE);
+            
+            EVP_CIPHER_CTX_init(ctx);
+            status = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr,
+                                        key_bytes.data(), iv_bytes.data());
+            CHECK_AES_STATUS(status, ctx, "[OpenSSL] EVP_DecryptInit_ex Error");
+            
+            EVP_CIPHER_CTX_set_key_length(ctx, EVP_MAX_KEY_LENGTH);
+            
+            /*** Decryption ***/
+            
+            int p_len = (int)input_content.size();
+            int f_len = 0;
+            std::vector<unsigned char> result_buffer(p_len);
+            
+            status = EVP_DecryptUpdate(ctx,
+                                       result_buffer.data(), &p_len,
+                                       input_content.data() + AES_INIT_VECTOR_SIZE,
+                                       (int) input_content.size() - AES_INIT_VECTOR_SIZE);
+            CHECK_AES_STATUS(status, ctx, "[OpenSSL] EVP_DecryptUpdate Error");
+            
+            status = EVP_DecryptFinal_ex(ctx, result_buffer.data() + p_len, &f_len);
+            CHECK_AES_STATUS(status, ctx, "[OpenSSL] EVP_DecryptFinal_ex Error");
+            
+            /*** Cleanup ***/
+            
+            EVP_CIPHER_CTX_cleanup(ctx);
+            EVP_CIPHER_CTX_free(ctx);
+            
+            int result_len = p_len + f_len;
+            result_buffer.resize(result_len);
+            
+            input_content = result_buffer;
+            
+            return Status::OK();
+        }
+    }
+
+}
