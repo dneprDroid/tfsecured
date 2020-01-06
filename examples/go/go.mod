55a0e32cbec1ab55e921aef306a65d146b3ebf7d
user <ovechko.056@gmail.com>
2020-01-06 14:51:00 +0000

    Add examples

---
+++ examples/go/go.mod
@@ -0,0 +1,6 @@
+module example
+
+go 1.13
+
+require github.com/tensorflow/tensorflow v1.5.0
+require github.com/dneprDroid/tfsecured v0.0.2
---
+++ examples/go/main.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+	"log"
+
+	tfsecured "github.com/dneprDroid/tfsecured/tfsecured/go"
+)
+
+func main() {
+	key := "BXKE0351PD9TXZ7XA8CK8XZU8XBGDM"
+	path := "../../python/demo/models/saved_model-encrypted.pb"
+
+	decryptor, err := tfsecured.NewDecryptorFile(key, path)
+	if err != nil {
+		log.Fatalf("NewDecryptorFile failed: %v", err)
+	}
+	if _, err := decryptor.Decrypt(); err != nil {
+		log.Fatalf("Decrypt failed: %v", err)
+	}
+}
